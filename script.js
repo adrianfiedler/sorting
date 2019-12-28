54f618e0b3b1e08b42daf918face847f06963afa
Adrian <adrian_fiedler@msn.com>
2019-12-28 13:24:01 +0100

    setup requirejs loading

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 requirejs(["p5"], function (p5) {
-  var myp5 = new p5(function (sketch) {
+  let myP5 = new p5(function (sketch) {
     const canvasWidth = 800;
     const canvasHeight = 800;
     const squareSize = 20;
@@ -12,7 +12,7 @@ requirejs(["p5"], function (p5) {
       for (let i = 0; i < cols; i++) {
         colors.push(new Array());
         for (let j = 0; j < rows; j++) {
-          colors[i].push(color(255, 255, 255));
+          colors[i].push(sketch.color(255, 255, 255));
         }
       }
     }
@@ -26,24 +26,25 @@ requirejs(["p5"], function (p5) {
           const x = col * squareSize;
           const y = row * squareSize;
 
-          fill(colors[col][row]);
-          stroke(0);
+          sketch.fill(colors[col][row]);
+          sketch.stroke(0);
           // For every column and row, a rectangle is drawn at an (x,y) location scaled and sized by videoScale.
-          rect(x, y, squareSize, squareSize);
+          sketch.rect(x, y, squareSize, squareSize);
         }
       }
     }
 
     function mouseDragged () {
-      if (mouseX > 0 && mouseX < canvasWidth && mouseY > 0 && mouseY < canvasHeight) {
-        const col = Math.floor(mouseX / squareSize);
-        const row = Math.floor(mouseY / squareSize);
+      if (sketch.mouseX > 0 && sketch.mouseX < canvasWidth && sketch.mouseY > 0 && sketch.mouseY < canvasHeight) {
+        const col = Math.floor(sketch.mouseX / squareSize);
+        const row = Math.floor(sketch.mouseY / squareSize);
 
-        colors[col][row] = color(0);
+        colors[col][row] = sketch.color(0);
       }
     }
 
     sketch.setup = setup;
+    sketch.draw = draw;
     sketch.mouseDragged = mouseDragged;
 
   }, 'sketchDiv');
