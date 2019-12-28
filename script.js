3ebd80994fae725d0a5f18bdf3a5007943dd382e
Adrian <adrian_fiedler@msn.com>
2019-12-28 14:10:39 +0100

    added QuickSort and integrated in drawing loop

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-requirejs(["libs/p5.min"], function (p5) {
-  let myP5 = new p5(function (sketch) {
+requirejs(['libs/p5.min', 'sorters/quicksort'], function (p5, QuickSort) {
+  new p5(function (sketch) {
     const canvasWidth = 800;
     const canvasHeight = 800;
     const squareSize = 20;
     const cols = Math.floor(canvasWidth / squareSize);
     const rows = Math.floor(canvasHeight / squareSize);
-    const colors = [];
+    const values = [];
 
     function setup () {
       const canvas = sketch.createCanvas(canvasWidth, canvasHeight);
       for (let i = 0; i < cols; i++) {
-        colors.push(new Array());
-        for (let j = 0; j < rows; j++) {
-          colors[i].push(sketch.color(255, 255, 255));
-        }
+        values.push(Math.random() * 400);
       }
+      sketch.frameRate(2);
+      let qs = new QuickSort(values);
+      qs.sort();
     }
 
     function draw () {
-      // Begin loop for columns
+      sketch.clear();
       for (let col = 0; col < cols; col++) {
-        // Begin loop for rows
-        for (let row = 0; row < rows; row++) {
+        const x = col * squareSize;
+        const y = 0;
 
-          const x = col * squareSize;
-          const y = row * squareSize;
-
-          sketch.fill(colors[col][row]);
-          sketch.stroke(0);
-          // For every column and row, a rectangle is drawn at an (x,y) location scaled and sized by videoScale.
-          sketch.rect(x, y, squareSize, squareSize);
-        }
-      }
-    }
-
-    function mouseDragged () {
-      if (sketch.mouseX > 0 && sketch.mouseX < canvasWidth && sketch.mouseY > 0 && sketch.mouseY < canvasHeight) {
-        const col = Math.floor(sketch.mouseX / squareSize);
-        const row = Math.floor(sketch.mouseY / squareSize);
-
-        colors[col][row] = sketch.color(0);
+        sketch.fill(sketch.color(255, 255, 255));
+        sketch.stroke(0);
+        // For every column and row, a rectangle is drawn at an (x,y) location scaled and sized by videoScale.
+        sketch.rect(x, y, squareSize, values[col]);
       }
     }
 
     sketch.setup = setup;
     sketch.draw = draw;
-    sketch.mouseDragged = mouseDragged;
 
   }, 'sketchDiv');
 
---
+++ sorters/quicksort.js
@@ -0,0 +1,56 @@
+define(function () {
+
+  class QuickSort {
+
+    constructor(list, sortStepCompleted) {
+      this.list = list;
+      this.sortStepCompleted = sortStepCompleted;
+    }
+    sort () {
+      this.quicksort(0, this.list.length - 1);
+    }
+
+    quicksort (links, rechts) {
+      if (links < rechts) {
+        const teiler = this.teile(links, rechts);
+
+        let that = this;
+        setTimeout(function () {
+          that.quicksort(links, teiler - 1);
+          that.quicksort(teiler + 1, rechts);
+        }, 500);
+      }
+
+    }
+
+    teile (links, rechts) {
+      let i = links;
+      let j = rechts - 1;
+      let pivot = this.list[rechts];
+
+      do {
+        while (i < rechts && this.list[i] < pivot) {
+          i++;
+        }
+        console.log(`found first i: ${i}`);
+
+        while (j > links && this.list[j] >= pivot) {
+          j--;
+        }
+        if (i < j) {
+          const temp = this.list[i];
+          this.list[i] = this.list[j];
+          this.list[j] = temp;
+        }
+        console.log(`found first j: ${j}`);
+      } while (i < j)
+      let temp = this.list[i];
+      this.list[i] = pivot;
+      this.list[rechts] = temp;
+      console.log(`teiler: ${i}`);
+      return i;
+    }
+  }
+
+  return QuickSort;
+});
